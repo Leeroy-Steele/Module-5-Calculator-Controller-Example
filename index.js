95484ab8ca5fd45bf00df71cd7971e4266bcdf47
Leeroy-Steele <leeroysteele@hotmail.com>
2023-04-05 20:54:44 +1200

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000   // 1024 -> 65535 are good port numbers for dev  
-const testRoute = require('./routes/myTestRoute')
+
+// require routes here
 const calculatorRoute = require('./routes/calculatorRoute')
 
 // create a route to a static html page
@@ -9,7 +10,6 @@ app.use('/', express.static('public'))
 app.use(express.json())
 
 // create a route
-app.use('/my-test-route', testRoute)
 app.use('/calculator', calculatorRoute)
 
 // start the server
